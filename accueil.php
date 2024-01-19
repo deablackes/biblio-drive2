3b80b4b6ab0b4010bffe2fd5f8794baeec791f84
deablackes <valentinpesrin1729@email.com>
2024-01-19 16:24:18 +0100

    zqydbyzq

--- accueil.php
+++ accueil.php
@@ -7,6 +7,49 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- Titre de la page -->
     <title>Accueil - Bibliodrive</title>
+    <script src="js/tarteaucitron/tarteaucitron.js"></script>
+
+<script>
+
+  tarteaucitron.init({
+
+    "privacyUrl": "", /* URL de la page de la politique de vie privée */
+
+    "hashtag": "#tarteaucitron", /* Ouvrir le panneau contenant ce hashtag */
+
+    "cookieName": "tarteaucitron", /* Nom du Cookie */
+
+    "orientation": "middle", /* Position de la bannière (top - bottom) */
+
+    "showAlertSmall": false, /* Voir la bannière réduite en bas à droite */
+
+    "cookieslist": false, /* Voir la liste des cookies */
+
+    "adblocker": false, /* Voir une alerte si un bloqueur de publicités est détecté */
+
+    "AcceptAllCta": true, /* Voir le bouton accepter tout (quand highPrivacy est à true) */
+
+    "highPrivacy": true, /* Désactiver le consentement automatique : OBLIGATOIRE DANS l'UE */
+
+    "handleBrowserDNTRequest": false, /* Si la protection du suivi du navigateur est activée, tout interdire */
+
+    "removeCredit": false, /* Retirer le lien vers tarteaucitron.js */
+
+    "moreInfoLink": true, /* Afficher le lien "voir plus d'infos" */
+
+    "useExternalCss": false, /* Si false, tarteaucitron.css sera chargé */
+
+    //"cookieDomain": ".my-multisite-domaine.fr", /* Cookie multisite - cas où SOUS DOMAINE */
+
+    "readmoreLink": "/cookiespolicy" /* Lien vers la page "Lire plus" A FAIRE OU PAS  */
+
+  });
+
+  (tarteaucitron.job = tarteaucitron.job || []).push('youtube');
+  <div class="youtube_player" videoID="PJCvmeRILLk" width="560" height="315" theme="light" rel="0" controls="1" showinfo="1" autoplay="0"></div>
+
+
+</script>
 </head>
 <body>
 
